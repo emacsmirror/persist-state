982e2fdea1b0d932457bf43f60cf1fc6ef858c32
Bram Schoenmakers <me@bramschoenmakers.nl>
2023-06-03 18:03:34 +0200

    Apply license properly

--- persist-state.el
+++ persist-state.el
@@ -22,6 +22,17 @@
 ;; copies of the Software, and to permit persons to whom the Software is
 ;; furnished to do so, subject to the following conditions:
 
+;; The above copyright notice and this permission notice shall be included in all
+;; copies or substantial portions of the Software.
+
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+;; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+;; SOFTWARE.
+
 ;;; Commentary:
 
 ;; See README.org.
@@ -113,8 +124,8 @@ is added as-is, otherwise it's wrapped in a lambda performing an
   (when (null (timerp persist-state--save-state-timer))
     (setq persist-state--save-state-timer
           (persist-state--regularly-run-on-idle persist-state-save-interval
-                                   persist-state-wait-idle
-                                   #'persist-state--save-state))))
+                                                persist-state-wait-idle
+                                                #'persist-state--save-state))))
 
 (defun persist-state--disable ()
   "Stop saving the Emacs state."
