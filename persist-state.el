d98f4bfb2ff3d8fdc5ab2c52372fbec90a9b700f
Bram Schoenmakers <me@bramschoenmakers.nl>
2023-06-12 23:39:34 +0200

    Adjust commentary a bit

--- persist-state.el
+++ persist-state.el
@@ -35,10 +35,9 @@
 
 ;;; Commentary:
 
-;; This package saves your Emacs state regularly when idle for a brief
-;; moment, to minimize disturbance of executing all save functions in
-;; sequence. In case Emacs is idle for a longer time, no state is
-;; saved.
+;; This package regularly saves your Emacs state (bookmarks, history,
+;; recent files, etc.) when idle for a brief moment. In case Emacs is
+;; idle for a longer time, no state is saved.
 
 ;; See README.org for more details.
 
